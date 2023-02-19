21f709a6fa578742a712b616786006052766960e
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2023-02-19 22:06:29 +0200

    make model carservice

--- app/Models/CarService.php
+++ app/Models/CarService.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class CarService extends Model
 {
     use HasFactory;
+    protected $guarded = ['id','created_at','updated_at'];
+
+    protected $appends = ['name'];
+
+    public function getNameAttribute()
+    {
+        if ( \app()->getLocale() == "ar") {
+            return $this->name_ar;
+        } else {
+            return $this->name_en;
+        }
+    }
+
+    public function scopeActive($query): void
+    {
+        $query->where('status', 'active');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(CarService::class, 'parent_id');
+    }
 }
