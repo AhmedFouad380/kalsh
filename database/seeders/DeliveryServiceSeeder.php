f691b790d9b33ef94aac2eb3f0441626773c8de9
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2023-03-08 14:11:48 +0200

    sd

--- database/seeders/DeliveryServiceSeeder.php
+++ database/seeders/DeliveryServiceSeeder.php
@@ -16,6 +16,7 @@ class DeliveryServiceSeeder extends Seeder
     {
         $data = [
             [
+                'id' => 1,
                 'name_ar' => 'توصيل طلبات',
                 'name_en' => 'Delivery orders',
                 'commission' => 10,
@@ -26,6 +27,21 @@ class DeliveryServiceSeeder extends Seeder
                 'range_provider' => 15,
                 'range_provider_to_shop' => 10,
                 'type' => 'delivery',
+                'status' => 'active',
+                'image' => 'delivery.png',
+            ],
+            [
+                'id' => 2,
+                'name_ar' => 'توصيل طرود',
+                'name_en' => 'Delivery packages',
+                'commission' => 10,
+                'min_cost' => 15,
+                'kilo_cost' => 1,
+                'min_distance' => 4,
+                'range_shop' => 30,
+                'range_provider' => 15,
+                'range_provider_to_shop' => 10,
+                'type' => 'package',
                 'status' => 'active',
                 'image' => 'delivery.png',
             ],
