e47890931ad4956e73925a769569a02b349b044b
MostafaElebzary <mostafaelebzary@gmail.com>
2023-03-11 19:01:03 +0200

    nearest provider

--- app/Helpers/ResearchProvidersTrait.php
+++ app/Helpers/ResearchProvidersTrait.php
@@ -3,6 +3,7 @@
 namespace App\Helpers;
 
 use App\Models\Notification;
+use App\Models\Order;
 use App\Models\Provider;
 use Illuminate\Support\Facades\DB;
 
@@ -11,11 +12,11 @@ trait ResearchProvidersTrait
     public function readyServiceProviders($order)
     {
         // get providers in order radius
-        $providers = $this->nearestProviders($order->from_lat,$order->from_lng,$order->radius,$order);
+        $providers = $this->nearestProviders($order->from_lat, $order->from_lng, $order->radius, $order);
 
-        if (!$providers->isEmpty()){
-            foreach ($providers as $provider){
-                $this->sendProviderNotification($order,$provider);
+        if (!$providers->isEmpty()) {
+            foreach ($providers as $provider) {
+                $this->sendProviderNotification($order, $provider);
             }
         }
     }
@@ -23,26 +24,27 @@ trait ResearchProvidersTrait
     public function CarServiceProviders($order)
     {
         // get providers in order radius
-        $providers = $this->nearestProviders($order->from_lat,$order->from_lng,$order->radius,$order);
+        $providers = $this->nearestProviders($order->from_lat, $order->from_lng, $order->radius, $order);
 
-        if (!$providers->isEmpty()){
-            foreach ($providers as $provider){
-                $this->sendProviderNotification($order,$provider);
+        if (!$providers->isEmpty()) {
+            foreach ($providers as $provider) {
+                $this->sendProviderNotification($order, $provider);
             }
         }
     }
+
     public function dreamServiceProvider($order)
     {
-        if ($order->provider){
-            $this->sendProviderNotification($order,$order->provider);
+        if ($order->provider) {
+            $this->sendProviderNotification($order, $order->provider);
         }
     }
 
-    public function nearestProviders($lat, $lng, $radius, $order=null)
+    public function nearestProviders($lat, $lng, $radius, $order = null, $service = null)
     {
         $providers = Provider::active()
             ->online()
-            ->select('providers.*',DB::raw('
+            ->select('providers.*', DB::raw('
                               6371 * ACOS(
                                   LEAST(
                                     1.0,
@@ -54,29 +56,32 @@ trait ResearchProvidersTrait
                                   )
                                 ) as distance'))
             ->having("distance", "<", $radius)
-            ->when($order,function ($query) use ($order){
-                $query->whereHas('providerServices',function ($query2) use ($order){
-                    $query2->where('service_id',$order->service_id);
+            ->when($order, function ($query) use ($order) {
+                $query->whereHas('providerServices', function ($query2) use ($order) {
+                    $query2->where('service_id', $order->service_id);
                 })
-                    ->when(!empty($order->ready_service_id),function ($query2) use ($order){
-                        $query2->whereHas('providerReadyServices',function ($query3) use ($order){
-                            $query3->where('ready_service_id',$order->ready_service_id);
+                    ->when(!empty($order->ready_service_id), function ($query2) use ($order) {
+                        $query2->whereHas('providerReadyServices', function ($query3) use ($order) {
+                            $query3->where('ready_service_id', $order->ready_service_id);
                         });
                     });
+            })->when($service && $service == "dream", function ($query4) {
+                $query4->whereHas('providerReadyServices', function ($query5) {
+                    $query5->where('service_id', 5);
+                });
             })
-
-            ->orderBy("distance",'asc')
+            ->orderBy("distance", 'asc')
             ->get();
         return $providers;
     }
 
-    public function sendProviderNotification($order,$provider)
+    public function sendProviderNotification($order, $provider)
     {
         $title_ar = 'طلب جديد';
         $title_en = 'New Order';
         $msg_ar = 'لديك طلب جديد بالقرب منك.';
         $msg_en = 'You have a new order near you.';
-        sendToProvider([$provider->device_token],${'title_'.$provider->lang},${'msg_'.$provider->lang},Notification::NEW_ORDER_TYPE,$order->id,$order->type);
+        sendToProvider([$provider->device_token], ${'title_' . $provider->lang}, ${'msg_' . $provider->lang}, Notification::NEW_ORDER_TYPE, $order->id, $order->type);
 
         Notification::create([
             'type' => Notification::NEW_ORDER_TYPE,
