3136bcc637f54f040ea19490b198ec864fcc37a0
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2023-03-12 20:02:40 +0200

    A

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,7 +33,8 @@ class AppServiceProvider extends ServiceProvider
             session()->put('lang','en');
         }
 
-        Artisan::call('migrate --seed');
+        Artisan::call('migrate');
+        Artisan::call('db:seed --class=DeliveryServiceSeeder');
         ob_start();
         Schema::defaultStringLength(191);
         date_default_timezone_set('Asia/Riyadh');
